7008220c1be78f2c44cf59d5be4af17a8b84057f
yam ferreira <yamferreira6@gmail.com>
2026-03-03 23:09:17 -0300

    feat: add method publish

--- src/main/java/com/example/categoria_produto/services/ProductService.java
+++ src/main/java/com/example/categoria_produto/services/ProductService.java
@@ -1,13 +1,12 @@
 package com.example.categoria_produto.services;
 
-import com.example.categoria_produto.domain.category.Category;
-import com.example.categoria_produto.domain.category.CategoryDTO;
 import com.example.categoria_produto.domain.category.exceptions.CategoryNotFoundException;
 import com.example.categoria_produto.domain.product.Product;
 import com.example.categoria_produto.domain.product.ProductDTO;
 import com.example.categoria_produto.domain.product.exceptions.ProductNotFoundException;
-import com.example.categoria_produto.repositories.CategoryRepository;
 import com.example.categoria_produto.repositories.ProductRepository;
+import com.example.categoria_produto.services.aws.AwsSnsService;
+import com.example.categoria_produto.services.aws.MessageDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,36 +14,43 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
 
-    private ProductRepository repository;
+    private final ProductRepository repository;
 
-    public ProductService(CategoryService categoryService, ProductRepository productRepository) {
+    private final AwsSnsService snsService;
+
+    public ProductService(CategoryService categoryService, ProductRepository productRepository, AwsSnsService snsService) {
         this.categoryService = categoryService;
         this.repository = productRepository;
+        this.snsService = snsService;
     }
 
     public Product insert(ProductDTO productData) {
-        Category category = this.categoryService.getById(productData.categoryId())
+        this.categoryService.getById(productData.categoryId())
                 .orElseThrow(CategoryNotFoundException::new);
         Product newProduct = new Product(productData);
-        newProduct.setCategory(category);
+
         this.repository.save(newProduct);
+        this.snsService.publish(new MessageDTO(newProduct.toString()));
         return newProduct;
     }
 
     public Product update(String id, ProductDTO productData) {
         Product product = this.repository.findById(id)
                 .orElseThrow(ProductNotFoundException::new);
-
-        this.categoryService.getById(productData.categoryId())
-                .ifPresent(product::setCategory);
+        if(productData.categoryId() != null) {
+            this.categoryService.getById(productData.categoryId());
+            product.setCategory(productData.categoryId());
+        }
 
         if (!productData.title().isEmpty()) product.setTitle(productData.title());
         if (!productData.description().isEmpty()) product.setDescription(productData.description());
         if (!(productData.price() == null)) product.setPrice(productData.price());
 
         this.repository.save(product);
+
+        this.snsService.publish(new MessageDTO(product.toString()));
 
         return product;
     }
